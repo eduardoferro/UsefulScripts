827e9925db8794a90263a950956c7443382169af
eduardoferro <edduardofe@hotmail.es>
2019-04-24 17:18:49 +0200

    Added a is charging condition

--- battery_alarm.sh
+++ battery_alarm.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 VAL="0"
 while :; do
+	ISCHARG=`cat /sys/class/power_supply/axp288_fuel_gauge/status`
+	if [ $ISCHARG == "Charging" ]; then
+		continue
+	fi
 	BAT=`cat /sys/class/power_supply/axp288_fuel_gauge/capacity`
 	LIMIT="10"
 	if [ $LIMIT -gt $BAT ]; then
